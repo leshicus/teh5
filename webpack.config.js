e51b3da83be2839409d2104aba8caa5af323da6e
leshicus <leshicus@gmail.com>
2016-03-03 20:57:25 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ import webpack from 'webpack';
 import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-const developmentEnvironment = 'development' ;
+const developmentEnvironment = 'development';
 const productionEnvironment = 'production';
 const testEnvironment = 'test';
 
-const getPlugins = function (env) {
+const getPlugins = function(env) {
   const GLOBALS = {
     'process.env.NODE_ENV': JSON.stringify(env),
     __DEV__: env === developmentEnvironment
@@ -33,13 +33,20 @@ const getPlugins = function (env) {
       break;
   }
 
+  plugins.push(
+    new webpack.ProvidePlugin({
+      'jQuery': 'jquery',
+      '$': 'jquery'
+    })
+  );
+
   return plugins;
 };
 
-const getEntry = function (env) {
+const getEntry = function(env) {
   const entry = [];
 
-  if (env === developmentEnvironment ) { // only want hot reloading when in dev.
+  if (env === developmentEnvironment) { // only want hot reloading when in dev.
     entry.push('webpack-hot-middleware/client');
   }
 
@@ -48,15 +55,20 @@ const getEntry = function (env) {
   return entry;
 };
 
-const getLoaders = function (env) {
+const getLoaders = function(env) {
   const loaders = [{ test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel', 'eslint'] }];
 
-  if (env === productionEnvironment ) {
+  if (env === productionEnvironment) {
     // generate separate physical stylesheet for production build using ExtractTextPlugin. This provides separate caching and avoids a flash of unstyled content on load.
-    loaders.push({test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract("css?sourceMap!sass?sourceMap")});
+    //loaders.push({ test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract("css?sourceMap!sass?sourceMap") });
+    loaders.push({ test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract("css?!sass?") });
   } else {
-    loaders.push({test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']});
+    //loaders.push({ test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap'] });
+    loaders.push({ test: /(\.css|\.scss)$/, loaders: ['style', 'css?', 'sass?'] });
   }
+
+  loaders.push({ test: /.(woff|woff2|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' });
+  loaders.push({ test: /images\/.*\.(png|jpg|svg|gif)$/, loader: 'url-loader?limit=10000&name="[name]-[hash].[ext]"' });
 
   return loaders;
 };
@@ -64,18 +76,23 @@ const getLoaders = function (env) {
 function getConfig(env) {
   return {
     debug: true,
-    devtool: env === productionEnvironment  ? 'source-map' : 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+    devtool: env === productionEnvironment ? 'source-map' : 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
     noInfo: true, // set to false to see a list of every file being bundled.
     entry: getEntry(env),
     target: env === testEnvironment ? 'node' : 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
     output: {
       path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-      publicPath: '',
+      //publicPath: '',
+      publicPath: 'http://localhost:3000/', // Development server
+      // publicPath: "http://teh5.ru/", // Production
       filename: 'bundle.js'
     },
     plugins: getPlugins(env),
     module: {
       loaders: getLoaders(env)
+    },
+    resolveLoader: {
+      root: path.join(__dirname, 'node_modules')
     }
   };
 }
